59a395811797d6383a1b8c6492974b757c180974
Firouzfar <firouzfar.sebastien@gmail.com>
2020-11-18 16:36:35 +0100

    End exo 5

--- condition.php
+++ condition.php
@@ -70,7 +70,80 @@ elseif ($heure == 21 || $minutes > 1  && $heure == 4 || $minutes == 59) {
     echo " Good night !"; 
 }
 // Test the value of $now and display the right message according to the specifications.
+?>
 
+<!--Exercice age people-->
+<form method="get" action="">
+	<label for="">...</label>
+	<input type="" name="age">
+	<input type="submit" name="submit" value="Greet me now">
+</form>
+
+<?php
+if (isset($_GET['age'])){
+    // Form processing
+    if($_GET['age'] == 12){
+        echo "Hello kiddo"; 
+    }elseif ($_GET['age'] >=12 && $_GET['age'] < 18) {
+        echo "Hello teenager"; 
+    }elseif ($_GET['age'] > 18 && $_GET['age'] < 115) {
+        echo "Hello adult"; 
+    }else{
+        echo "Wow! Still alive ? Are you a robot, like me ? Can I hug you ?";
+    }
+}
+?>
+
+<!--Exo 4-->
+<form method="get" action="">
+	<label for="language">Do you speak english ?</label>
+	<input type="" name="agePeople">
+    
+    <label for="yes">yes</label>
+    <input type="radio" name="language" value = "yes" >
+    
+    <label for="no">no</label>
+    <input type="radio"  name="language" value = "no" >
+	<input type="submit" name="submit" value="Greet me now">
+</form>
+<?php
+
+if (isset($_GET['language'])){
+    // Form processing
+    if($_GET['language'] < 12 && $_GET['language'] == "yes"){
+        echo "Hello boy! "; 
+    }else {
+        echo "Aloha boy"; 
+    }
+}
+?>
+
+
+<!--Exo 5-->
+<form method="get" action="">
+	<label for="soccers">Give me your information </label>
+    
+    <label for="yes">Name </label>
+    <input type="text" name="soccers" value = "namePlayers" >
+
+    <label for="">Age</label>
+    <input type="text" name="age" value = "age" >
+    
+    <label for="">Gender</label>
+     <input type="radio"  name="gender" value = "femelle" > femalle
+     <input type="radio"  name="gender" value = "man" > man
+	<input type="submit" name="submit" value="Greet me now">
+</form>
+
+<?php 
+    if(isset($_GET['soccers'])){
+        if($_GET['age'] > 21 && $_GET['age'] < 40 && $_GET['gender'] == "femelle" ){
+            echo "welcome to the team !"; 
+        }else{
+            echo "Sorry you don't fit the criteria";
+        }
+    }
+  
 
 ?>
 
