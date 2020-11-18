e9942f0e0040747970ef5531ad0b079b3b7f659a
Firouzfar <firouzfar.sebastien@gmail.com>
2020-11-18 12:27:22 +0100

    fin Exo 2.2

--- condition.php
+++ condition.php
@@ -8,6 +8,21 @@
 <body>
 
 <?php 
+
+$room_is_filthy = true;
+if($room_is_filthy == true){
+    echo "Yuk, Room is dirty : let's clean it up !";
+    
+    echo "<br>Room is now clean!";
+    $room_is_filthy = false;
+} else {
+echo "<br>Nothing to do, room is neat.";
+}
+
+
+?>
+
+<?php 
    $possible_states = [" danger pour la santé", "sale", "propre", "immaculée"];
 
    // When testing, change the index value to navigate to the possible array values
@@ -23,6 +38,42 @@
        echo "<br>Nothing to do, room is neat.";
        }
 ?>
-    
+
+<?php 
+//affiche l'horaire local 
+date_default_timezone_set('Europe/Paris');
+$heure = date("G") ; // How to get the current time in PHP ? Google is your friend ;-)
+$minutes = date("i");
+echo "$heure". " : " ."$minutes";
+
+// 05h00 and 09h00
+if($heure == 5 && $heure == 9){
+    echo " Good morning";
+
+    // 09h01 and 12h00
+} elseif ($heure == 9 || $minutes > 1  && $heure < 12) {
+    echo " Good day"; 
+
+//12h01 and 16h00
+}elseif ($heure == 12 || $minutes > 1  && $heure < 16) {
+    echo " Good afternoon !"; 
+}
+
+//16h01 and 21h00 
+elseif ($heure == 16 || $minutes > 1  && $heure < 21) {
+    echo " Good evening  !"; 
+}
+
+
+//21h01 and 4h59 
+elseif ($heure == 21 || $minutes > 1  && $heure == 4 || $minutes == 59) {
+    echo " Good night !"; 
+}
+// Test the value of $now and display the right message according to the specifications.
+
+
+?>
+
+
 </body>
 </html>
--- variable_type.php
+++ variable_type.php
@@ -32,18 +32,6 @@
         }else{
             echo $isFalse;
         }
-
-        
-
-        $room_is_filthy = true;
-    if($room_is_filthy == true){
-	    echo "Yuk, Room is dirty : let's clean it up !";
-	    
-	    echo "<br>Room is now clean!";
-	    $room_is_filthy = false;
-    } else {
-	echo "<br>Nothing to do, room is neat.";
-    }
 ?>
 
 
