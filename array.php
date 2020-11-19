e009d7ca41682e3e03c42f7c638df6609e2b15c1
Firouzfar <firouzfar.sebastien@gmail.com>
2020-11-19 16:32:48 +0100

    suite en fin array

--- array.php
+++ array.php
@@ -6,15 +6,52 @@ echo "$countries[2] </br>";
 $familly = array('Sebastien', 'Aidin' , 'Alice', 'Morgane', 'Laurine');
 print_r($familly);
 
+echo  '</br>';
 $recipes = array('Khoubideh', 'Nouille' , 'Sushi', 'Pizza', 'Patte');
 print_r($recipes);
 
 $favoriteMovie = array('Fast And furious', 'Saw' , 'Conjuring', 'Spiderman');
-
 print_r($favoriteMovie[2]);
 
 echo '<pre>';
 print_r($countries);
 echo '</pre>';
+
+var_dump($countries);
+
+array_push($countries, 'England');
+print_r($countries);
+
+$person['function'] = 'Junior Web Developer';
+echo $person['function'];
+?>
+
+<!--Exo1-->
+<?php 
+
+$me = array(
+    'firstname' => 'sebastien', 
+    "lastname" => "firouzfar",
+    "favoriteMovie" => array("spiderman", "conjuring", "matrix"),
+    "hobbies" => array("Play football", "travel")
+);
+
+
+echo '<pre>';
+print_r($me);
+echo '</pre>';
+
+$mother = array(
+    'firstname' => 'Jacline', 
+    "lastname" => "moms", 
+); 
+
+$me['hobbies'] = $mother; 
+ 
+
+echo '<pre>';
+print_r($me);
+echo '</pre>';
+
 
 ?>
